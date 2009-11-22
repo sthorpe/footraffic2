1a084de12b8822cb31f536b28b0aba51cf42c11a
Scott Thorpe <sthorpe@gmail.com>
2009-11-22 07:18:32 -0800

    now calling the consumer

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -2,7 +2,7 @@ class FoursquareController < ApplicationController
   
   def index
     @foursquare = Foursquare.new
-    @request_token = OAuth::RequestToken.new(@consumer, session[:oauth_request].token, session[:oauth_request].secret)
+    @request_token = OAuth::RequestToken.new(@foursquare.consumer, session[:oauth_request].token, session[:oauth_request].secret)
     @access_token = @request_token.get_access_token
   end
 end
