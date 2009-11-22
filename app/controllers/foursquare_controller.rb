5ef3e020538d2ca626c07f92ed434651b710a94e
Scott Thorpe <sthorpe@gmail.com>
2009-11-22 04:30:47 -0800

    Now grabbing oauth_token from url

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -1,6 +1,6 @@
 class FoursquareController < ApplicationController
   
   def index
-    @access_token = Foursquare.new().save_token
+    @access_token = Foursquare.new().save_token(params[:oauth_token])
   end
 end
