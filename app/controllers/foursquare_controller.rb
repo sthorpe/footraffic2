8d6b05a97ee5f4030cb31dc70201fdaf8fbe0dbb
Scott Thorpe <sthorpe@gmail.com>
2009-11-22 07:35:02 -0800

    Adding history to view

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -2,9 +2,9 @@ class FoursquareController < ApplicationController
   
   def index
     @foursquare = Foursquare.new
-    #@request_token = OAuth::RequestToken.new(@foursquare.consumer, session[:oauth_request_token], session[:oauth_request_secret])
-    #@access_token = @request_token.get_access_token
+    @request_token = OAuth::RequestToken.new(@foursquare.consumer, session[:oauth_request_token], session[:oauth_request_secret])
+    @access_token = @request_token.get_access_token
     @where_been = @foursquare.where_been
-    @access_token = OAuth::AccessToken.new(@where_been,session[:oauth_request_token], session[:oauth_request_secret])
+    @access = OAuth::AccessToken.new(@where_been,@access_token.token, @access_token.secret)
   end
 end
