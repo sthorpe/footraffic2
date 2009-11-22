13c67a711470382a414cd3daea098a1a32ff3043
Scott Thorpe <sthorpe@gmail.com>
2009-11-22 07:31:55 -0800

    Testing

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -2,8 +2,8 @@ class FoursquareController < ApplicationController
   
   def index
     @foursquare = Foursquare.new
-    @request_token = OAuth::RequestToken.new(@foursquare.consumer, session[:oauth_request_token], session[:oauth_request_secret])
-    @access_token = @request_token.get_access_token
+    #@request_token = OAuth::RequestToken.new(@foursquare.consumer, session[:oauth_request_token], session[:oauth_request_secret])
+    #@access_token = @request_token.get_access_token
     @where_been = @foursquare.where_been
     @access_token = OAuth::AccessToken.new(@where_been,session[:oauth_request_token], session[:oauth_request_secret])
   end
