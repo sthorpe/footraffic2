4d932cde06ee2a30df0e0bc256aa7baf1614f90b
DoktahWorm <doktahworm@gmail.com>
2009-12-05 23:11:53 -0800

    Updated key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_ratr_session',
-  :secret      => 'ab784050c80b4206a6ba0f5c208d788a2a0edd07f945d3fc5f266aa34b1ad7d55abf6635cb9e2ec0c8ceb'
+  :secret      => 'd98871ee5d723fa868787e3c3d928e348987878ea3f193c82ffacd87878be18842e297f440f2605b14457ce6427ba6ec887878739dff29f3d0a4587'
 }
 
 # Use the database for sessions instead of the cookie-based default,
